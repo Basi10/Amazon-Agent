78c206bcfc602191a1e8173f79a16ab27600e402
Basi10 <basilelbirru@gmail.com>
2024-05-17 18:02:38 +0300

    added directory constants

---
+++ script/constants.py
@@ -0,0 +1,9 @@
+class URL:
+    AMAZON_URL = 'https://www.amazon.com/'
+    
+class Directories:
+    IMAGE_DIRECTORY = './output/'
+    LOG_DIRECTORY = './output/browser_action.log'
+    EXCEL_DIRECTORY = './output/'
+    EXCEL_FILE_EXT = ".xlsx"
+    SUPPORTED_IMAGE_FORMATS = [".jpg", ".jpeg", ".png"]
